adc71890cf37e8da55576bfdcc3520f62b8bc947
Ajmi <109217309+ajmins@users.noreply.github.com>
2022-07-13 17:09:44 +0530

    Update mycode.py with some additional changes

--- mycode.py
+++ mycode.py
@@ -4,3 +4,4 @@ print("Hi everyone!")
 print("Hello wordl!")
 print("I am not sured about it and I am worried and i changed this line the master branch also");
 print("Changes made from the github editor!") 
+print("some additional changes to demonstrate fetch and pull seperately"
