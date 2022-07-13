8ec10d84893c2f44cc0cda166e5757d7ec527b2a
ajmins <ajmi.ns@inapp.com>
2022-07-13 17:25:50 +0530

    Trying to push the changes to remote github

--- mycode.py
+++ mycode.py
@@ -4,4 +4,5 @@ print("Hi everyone!")
 print("Hello wordl!")
 print("I am not sured about it and I am worried and i changed this line the master branch also");
 print("Changes made from the github editor!") 
-print("some additional changes to demonstrate fetch and pull seperately"
+print("some additional changes to demonstrate fetch and pull seperately")
+print("more additional changes in the local repo")
